c4a09cf3e02cf8fe83581882a3c30b6d57e1b6b1
Dinh Hoang Dung <dungdh@cyberspace.vn>
2023-10-04 22:30:16 +0700

    Service: change robot color

---
+++ devel/share/gennodejs/ros/robot_msgs/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let ABC = require('./ABC.js');
+let MoveToPoseActionFeedback = require('./MoveToPoseActionFeedback.js');
+let MoveToPoseFeedback = require('./MoveToPoseFeedback.js');
+let MoveToPoseActionGoal = require('./MoveToPoseActionGoal.js');
+let MoveToPoseActionResult = require('./MoveToPoseActionResult.js');
+let MoveToPoseResult = require('./MoveToPoseResult.js');
+let MoveToPoseGoal = require('./MoveToPoseGoal.js');
+let MoveToPoseAction = require('./MoveToPoseAction.js');
+
+module.exports = {
+  ABC: ABC,
+  MoveToPoseActionFeedback: MoveToPoseActionFeedback,
+  MoveToPoseFeedback: MoveToPoseFeedback,
+  MoveToPoseActionGoal: MoveToPoseActionGoal,
+  MoveToPoseActionResult: MoveToPoseActionResult,
+  MoveToPoseResult: MoveToPoseResult,
+  MoveToPoseGoal: MoveToPoseGoal,
+  MoveToPoseAction: MoveToPoseAction,
+};
--- src/robot_controller/include/robot_controller.h
+++ src/robot_controller/include/robot_controller.h
@@ -2,6 +2,7 @@
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/Twist.h>
+#include <std_srvs/SetBool.h>
 
 class RobotController
 {
@@ -11,6 +12,9 @@ class RobotController
         geometry_msgs::Pose2D robot_pose_;
         ros::Subscriber path_sub_, robot_pose_sub_;
         ros::Publisher cmd_vel_pub_;
+        ros::ServiceServer enable_srv;
+        bool enable_moving = false;
+
     public:
         RobotController()
         {
@@ -18,6 +22,7 @@ class RobotController
             robot_pose_sub_ = nh_.subscribe("robot_pose", 1, &RobotController::robotposeCB, this);
 
             cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+            enable_srv = nh_.advertiseService("enable_moving", &RobotController::enableCB, this);
         }
         ~RobotController()
         {
@@ -34,18 +39,31 @@ class RobotController
             ROS_INFO_STREAM("Robot pose: "<<robotpose.x<<"-"<<robotpose.y);
         }
 
+        bool enableCB(std_srvs::SetBool::Request& req,
+                      std_srvs::SetBool::Response& res)
+        {
+            enable_moving = req.data;
+            res.success = true;
+            res.message = "OK";
+            return true;
+        }
+
         void execute()
         {
             //code cua bo dieu khien
             geometry_msgs::Twist vel;
-            //van toc tinh tien
-            vel.linear.x = 1.0;
-            //toc do goc quay quanh truc z
-            vel.angular.z = 1.0;
+            if (enable_moving) {
+                //van toc tinh tien
+                vel.linear.x = 1.0;
+                //toc do goc quay quanh truc z
+                vel.angular.z = 1.0;
+            } else {
+                //van toc tinh tien
+                vel.linear.x = 0.0;
+                //toc do goc quay quanh truc z
+                vel.angular.z = 0.0;
+            }
+            
             cmd_vel_pub_.publish(vel);
         }
-
-
-
-
 };
